0a22ca209616587b77937632cdc1e1835218d5c4
b4ttl3C4t <sam00298653@gmail.com>
2023-11-10 13:10:37 +0800

    Update

--- Computer_Programming_Final_Term_Project/Computer_Programming_Final_Term_Project.c
+++ Computer_Programming_Final_Term_Project/Computer_Programming_Final_Term_Project.c
@@ -5,6 +5,7 @@
 
 #define BUF_SIZE    150
 #define DATA_WIDTH  5
+#define LOWER_WIDTH 29
 #define SWAP(x, y)  (x) = (x) ^ (y);    \
                     (y) = (x) ^ (y);    \
                     (x) = (x) ^ (y);
@@ -18,20 +19,20 @@ static int32_t  code        [BUF_SIZE] = {0};
 static int32_t  code_buf    [BUF_SIZE] = {0};
 static int32_t  data        [BUF_SIZE] = {0};
 static int32_t  data_index, each, i, j;
-static int32_t  narrow_format, wide_format;
+static double   narrow_format, wide_format;
 
 void            scan_code       (/*FILE *,*/ int32_t);
 void            sort_code       (int32_t);
 inline double   bias_lower      (double);
 inline double   bias_upper      (double);
 bool            take_format     (int32_t);
+inline bool     calibrate_bias  (int32_t);
 int32_t         raw_data        (int32_t);
-int32_t         reverse_data    (int32_t [], int32_t);
+int32_t         reverse_data    (int32_t);
 inline bool     boundary_check  (int32_t);
 inline bool     split_check     (int32_t);
 inline bool     C_check         (int32_t [], int32_t);
 inline bool     K_check         (int32_t [], int32_t);
-inline bool     calibrate_bias  (int32_t, int32_t *);
 inline bool     decode_table    (data_t *);
 inline int32_t  code_weight     (data_t *);
 
@@ -50,11 +51,27 @@ int main(void)
         {
             break;
         }
-        if(boundary_check(m))
+
+        if(!boundary_check(m))
         {
             printf("#Case%d : bad code\n", count);
             continue;
         }
+
+        scan_code(m);
+        sort_code(m);
+        
+        if(!take_format(m))
+        {
+            printf("#Case%d : bad code\n", count);
+            continue;
+        }
+        
+        calibrate_bias(m);
+
+        printf("%lf %lf\n", narrow_format, wide_format);
+
+        fgetc(stdin);
     }
     
     //fclose(file_stream);
@@ -87,32 +104,35 @@ void sort_code(int32_t m)
     }
 }
 
-double bias_lower(double m)
+double bias_lower(double x)
 {
-    return (m - m * 0.05);
+    return (x - x * 0.05);
 }
 
-double bias_upper(double m)
+double bias_upper(double x)
 {
-    return (m + m * 0.05);
+    return (x + x * 0.05);
 }
 
 bool take_format(int32_t m)
 {
-    static int32_t summation;
-    summation = narrow_format = wide_format = 0;
+    static double summation, break_point;
+    summation = 0.0;
+    narrow_format = wide_format = 0;
 
     for(each = 0; each < m - 1; ++each)
     {
         summation += code_buf[each];
         if((code_buf[each + 1] - code_buf[each]) > (bias_upper(code[each]) - bias_lower(code[each])))
         {
-            narrow_format = summation / each;
+            narrow_format = summation / (each + 1);
             break;
         }
     }
 
-    summation = 0;
+    summation = 0.0;
+    ++each;
+    break_point = each + 1;
     for(; each < m - 1; ++each)
     {
         summation += code_buf[each];
@@ -121,14 +141,14 @@ bool take_format(int32_t m)
             return 0;
         }
     }
-    wide_format = summation / each;
+    wide_format = summation / (each - break_point);
 
     return 1;
 }
 
-inline bool boundary_check(int32_t m)
+bool boundary_check(int32_t m)
 {
-    if(m % 6 == 5)
+    if((m % (DATA_WIDTH + 1) == DATA_WIDTH) && m >= LOWER_WIDTH)
     {
         return 1;
     }
@@ -138,9 +158,31 @@ inline bool boundary_check(int32_t m)
     }
 }
 
-inline bool     split_check     (int32_t);
-inline bool     C_check         (int32_t [], int32_t);
-inline bool     K_check         (int32_t [], int32_t);
-inline bool     calibrate_bias  (int32_t, int32_t *);
-inline bool     decode_table    (data_t *);
-inline int32_t  code_weight     (data_t *);
+inline bool calibrate_bias(int32_t m)
+{
+    for(each = 0; each < m; ++each)
+    {
+        if( bias_lower(narrow_format) < code[each] || 
+            bias_upper(narrow_format) > code[each])
+        {
+            code[each] = narrow_format + 0.5;
+            continue;
+        }
+
+        if( bias_lower(wide_format) < code[each] ||
+            bias_upper(wide_format) > code[each])
+        {
+            code[each] = wide_format + 0.5;
+            continue;
+        }
+
+        return 0;
+    }
+
+    for(each = 0; each < m - 1; ++each)
+    {
+        printf("%d ", code[each]);
+    }
+
+    return 1;
+}
