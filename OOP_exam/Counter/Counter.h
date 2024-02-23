c64fb5de59018cd9a7416fd2e67f776f641e83ba
b4ttl3C4t <129575894+b4ttl3C4t@users.noreply.github.com>
2024-02-23 20:20:43 +0800

    Add some case

--- OOP_exam/Counter/Counter.h
+++ OOP_exam/Counter/Counter.h
@@ -1,7 +1,6 @@
 #ifndef COUNTER_H
 #define COUNTER_H
 
-#include <iostream>
 #include <vector>
 #include <map>
 
@@ -13,21 +12,23 @@ class Counter: public map<T, int>
 public:
     Counter();
     Counter(vector<T> items);
-    vector<T> most_common();
-    vector<T> most_common(int n);
-    int       total();
-    void      subtract();
-    void      subtract(T key);
-    void      subtract(vector<T> keys);
-    vector<T> operator+(const Counter &x);
-    vector<T> operator-(const Counter &x);
-    Counter   operator+();
-    Counter   operator-();
+    vector<T>  most_common();
+    vector<T>  most_common(int n = -1);
+    int        total();
+    void       subtract();
+    void       subtract(T key);
+    void       subtract(vector<T> keys);
+    const Counter<T> operator+(const Counter &x);
+    const Counter<T> operator-(const Counter &x);
+    const Counter<T> operator+();
+    const Counter<T> operator-();
 private:
     Counter(const Counter &);
     Counter(const Counter &&);
     Counter& operator=(const Counter &);
     Counter& operator=(const Counter &&);
 };
+
+#include "Counter.cpp"
 
 #endif
--- OOP_exam/Counter/main.cpp
+++ OOP_exam/Counter/main.cpp
@@ -1,9 +1,19 @@
-#include "Counter.h"
 #include <iostream>
 #include <vector>
 
+#include "Counter.h"
+
 int main() {
     int myints[] = {1, 2, 3, 3, 2, 1, 2};
+
+    std::vector<int> items = {1, 2, 3, 4, 1, 2, 2, 3, 3, 3};
+    Counter<int> counter(items);
+
+    std::cout << "Most common elements:" << std::endl;
+    for (int item : counter.most_common(3)) {
+        std::cout << item << " ";
+    }
+
     std::vector<int> a(myints, myints + sizeof(myints) / sizeof(int));
     Counter<int> c(a);
     std::cout << c[99] << std::endl;
